a7046474efc38d3cd01feba6837828350280f40d
Zach Eggert <egger0a6@email.arizona.edu>
2022-06-14 00:47:05 -0400

    add data dir and files, add views, add dependencies

---
+++ data/nfl-players-data.js
@@ -0,0 +1,16 @@
+const nflPlayers = [
+  {name: "Jonathan Taylor", team: "IND", position: "RB"},
+  {name: "Derrick Henry", team: "TEN", position: "RB"},
+  {name: "Austin Ekeler", team: "LAC", position: "RB"},
+  {name: "Najee Harris", team: "PIT", position: "RB"},
+  {name: "Cooper Kupp", team: "LAR", position: "WR"},
+  {name: "Dalvin Cook", team: "MIN", position: "RB"},
+  {name: "Christian McCaffrey", team: "CAR", position: "RB"},
+  {name: "Justin Jefferson", team: "MIN", position: "WR"},
+  {name: "Nick Chubb", team: "CLE", position: "RB"},
+  {name: "Alvin Kamara", team: "NO", position: "RB"},
+];
+
+export {
+  nflPlayers
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+// import modules
+
+import express from 'express';
+import { nflPlayers } from './data/nfl-players-data.js';
+
+
+// Create Express app
+
+const app = express();
+
+
+// Configure the app (app.set)
+
+app.set("view engine", "ejs");
+
+
+// Mount Middleware (app.use)
+
+
+
+// Mount routes
+
+app.get("/", function(req, res) {
+  res.redirect("/home");
+});
+
+app.get("/home", function(req, res) {
+  res.render("home");
+});
+
+app.get("/nfl-players", function(req, res) {
+  res.render("nfl-players/index", {
+    
+  });
+});
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
